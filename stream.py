89feceac69b1bc9f3f0fa47c3a98358ae45c7d7a
mooncrown04 <mooncrown04@gmail.com>
2026-04-24 21:23:44 +0300

    Update stream.py

--- stream.py
+++ stream.py
@@ -33,7 +33,7 @@ def process_stremio_addon(m3u_file):
     category_map = {
         "ulusal": "📺 Ulusal Kanallar",
         "spor": "⚽ Spor Dünyası",
-        "haber": "📰 Haber",
+        "haber": "📰 Haberler",
         "sinema": "🎬 Sinema & Dizi",
         "dizi": "🎬 Sinema & Dizi",
         "belgesel": "🦒 Belgesel & Yaşam",
