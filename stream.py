20dec35a19d428695c3de89d455c1683331f211f
mooncrown04 <mooncrown04@gmail.com>
2026-05-20 15:31:46 +0300

    Update stream.py

--- stream.py
+++ stream.py
@@ -75,6 +75,7 @@ def process_stremio_addon():
         "ANIMASYON": "🎨 Animasyon",
         "COCUK": "🧸 Çocuk",
         "YETISKIN": "🔞 Yetişkin",
+        "Touchtv Slovakia": "📡 YABANCI ","Freeshot Canada": "📡 YABANCI","FREESHOT": "📡 YABANCI",
         "DIGER": "📡 Diğer Kanallar"
     }
 
